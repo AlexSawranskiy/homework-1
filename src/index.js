a744a14df4999498daf6bb4a2602c94af116bd76
AlexSawranskiy <alexsawranskiy989@gmail.com>
2023-04-16 16:14:04 +0300

    Додає темну тему

--- src/index.js
+++ src/index.js
@@ -6,3 +6,24 @@ const foodServiceMarkUp = foodServiceTpl(menuData);
 
 const menuEl = document.querySelector('.js-menu');
 menuEl.insertAdjacentHTML('beforeend', foodServiceMarkUp);
+
+const switcherTheme = document.querySelector('.theme-switch__toggle');
+const theme = localStorage.getItem('currentTheme');
+switcherTheme.addEventListener('change', changeSwitcher)
+
+function changeSwitcher() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    const value = document.body.classList;
+    localStorage.setItem('currentTheme', value[1])
+}
+if (theme === null) {
+    localStorage.setItem('currentTheme', 'light-theme');
+    document.body.classList.add('light-theme')
+} else {
+    document.body.classList.add(theme)
+};
+if (theme === 'dark-theme') {
+    switcher.checked = true;
+}
